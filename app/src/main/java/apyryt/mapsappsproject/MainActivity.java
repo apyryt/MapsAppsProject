7b869bd1602fce035210116988efa4caf2f29548
Amanda Pyryt <amandapyryt@gmail.com>
2016-04-09 20:05:08 -0400

    changes

--- app/src/main/java/apyryt/mapsappsproject/MainActivity.java
+++ app/src/main/java/apyryt/mapsappsproject/MainActivity.java
@@ -2,15 +2,31 @@ package apyryt.mapsappsproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button addActivity;
+    private Button addActivity;
+    private EditText titleText, descriptionText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //button
+        addActivity = (Button) findViewById(R.id.addEventButton);
+        addActivity.setOnClickListener(this);
+
+        //textboxes to enter info for app
+        titleText = (EditText) findViewById(R.id.titleEditText);
+        descriptionText = (EditText) findViewById(R.id.descriptionEditText);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
